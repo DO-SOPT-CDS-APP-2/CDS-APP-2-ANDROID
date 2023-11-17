2d26a8fa5d5f09ac5bd16db6ff6606ea3fd7205d
Sangho Kim <marchbreeze03@gmail.com>
2023-11-17 23:01:03 +0900

    [ADD/#1] Manifest 설정

---
+++ app/src/main/java/org/sopt/cds29cm/presentation/MockActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.cds29cm.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.sopt.cds29cm.R
+
+class MockActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mock)
+    }
+}
