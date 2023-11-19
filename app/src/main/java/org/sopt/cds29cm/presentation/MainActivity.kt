45c2151fa13f91a92483e1098643d127504d9f4c
LEE YOU BIN <youbeene3409@naver.com>
2023-11-20 01:36:25 +0900

    [FEAT/#5] NavigationBar 구현

--- app/src/main/java/org/sopt/cds29cm/presentation/MainActivity.kt
+++ app/src/main/java/org/sopt/cds29cm/presentation/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        replaceFragment(HomeFragment())
         clickBottomNavigation()
     }
     private fun clickBottomNavigation(){
