a34f63486d6d65d1b241cd846f3e5910c845e8ff
Sangho Kim <marchbreeze03@gmail.com>
2023-11-19 22:13:40 +0900

    [ADD/#4] HatDetail뷰 액티비티 생성

---
+++ app/src/main/java/org/sopt/cds29cm/presentation/hatDetail/HatDetailActivity.kt
@@ -0,0 +1,15 @@
+package org.sopt.cds29cm.presentation.hatDetail
+
+import android.os.Bundle
+import org.sopt.cds29cm.R
+import org.sopt.cds29cm.util.base.BindingActivity
+import org.sopt.cds29cm.databinding.ActivityHatDetailBinding
+
+class HatDetailActivity : BindingActivity<ActivityHatDetailBinding>(R.layout.activity_hat_detail) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
