2761be290579737415208611e5d6065ecc09ddba
LEE YOU BIN <youbeene3409@naver.com>
2023-11-20 02:10:24 +0900

    [DEL/#5] 불필요한 화면 삭제

--- app/src/main/java/org/sopt/cds29cm/presentation/MainActivity.kt
+++ app/src/main/java/org/sopt/cds29cm/presentation/MainActivity.kt
@@ -7,10 +7,6 @@ import org.sopt.cds29cm.R
 import org.sopt.cds29cm.databinding.ActivityMainBinding
 import org.sopt.cds29cm.presentation.category.CategoryFragment
 import org.sopt.cds29cm.presentation.home.HomeFragment
-import org.sopt.cds29cm.presentation.like.LikeFragment
-import org.sopt.cds29cm.presentation.my.MyFragment
-import org.sopt.cds29cm.presentation.search.SearchFragment
-
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,18 +26,6 @@ class MainActivity : AppCompatActivity() {
                 }
                 R.id.menu_category -> {
                     replaceFragment(CategoryFragment())
-                    true
-                }
-                R.id.menu_search -> {
-                    replaceFragment(SearchFragment())
-                    true
-                }
-                R.id.menu_like -> {
-                    replaceFragment(LikeFragment())
-                    true
-                }
-                R.id.menu_my -> {
-                    replaceFragment(MyFragment())
                     true
                 }
                 else -> false
