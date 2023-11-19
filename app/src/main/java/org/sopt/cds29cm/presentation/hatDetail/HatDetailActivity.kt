8aa67a4af789c56484c6648b2f61b55deb9967aa
Sangho Kim <marchbreeze03@gmail.com>
2023-11-20 04:54:37 +0900

    [FIX/#4] Multi-view Type 삭제

--- app/src/main/java/org/sopt/cds29cm/presentation/hatDetail/HatDetailActivity.kt
+++ app/src/main/java/org/sopt/cds29cm/presentation/hatDetail/HatDetailActivity.kt
@@ -7,23 +7,8 @@ import org.sopt.cds29cm.util.base.BindingActivity
 
 class HatDetailActivity : BindingActivity<ActivityHatDetailBinding>(R.layout.activity_hat_detail) {
 
-    private var _adapter: HatDetailAdapter? = null
-    private val adapter
-        get() = requireNotNull(_adapter) { getString(R.string.adapter_not_initialized_error_msg) }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        initAdapter()
-    }
-
-    private fun initAdapter() {
-        _adapter = HatDetailAdapter(this.baseContext)
-        binding.rvHatDetail.adapter = adapter
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        _adapter = null
     }
 }
