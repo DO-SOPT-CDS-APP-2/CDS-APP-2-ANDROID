d32de6f0359e71e55ebf351f7b73c0f1097ef901
Sangho Kim <marchbreeze03@gmail.com>
2023-11-21 02:53:25 +0900

    [FEAT/#4] 다른 탭들 작동 방지

--- app/src/main/java/org/sopt/cds29cm/presentation/hatDetail/HatDetailActivity.kt
+++ app/src/main/java/org/sopt/cds29cm/presentation/hatDetail/HatDetailActivity.kt
@@ -1,7 +1,6 @@
 package org.sopt.cds29cm.presentation.hatDetail
 
 import android.os.Bundle
-import android.view.View
 import com.google.android.material.tabs.TabLayout
 import org.sopt.cds29cm.R
 import org.sopt.cds29cm.databinding.ActivityHatDetailBinding
@@ -9,35 +8,35 @@ import org.sopt.cds29cm.util.base.BindingActivity
 import org.sopt.cds29cm.util.extension.computeDistanceToView
 import org.sopt.cds29cm.util.extension.smoothScrollToView
 
-class HatDetailActivity : BindingActivity<ActivityHatDetailBinding>(R.layout.activity_hat_detail),
-    TabLayout.OnTabSelectedListener, View.OnScrollChangeListener {
+class HatDetailActivity : BindingActivity<ActivityHatDetailBinding>(R.layout.activity_hat_detail) {
 
     private val tabTextList = listOf("상품정보", "사이즈", "추천", "리뷰", "문의")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        initTabLayoutScrollListener()
-        setTabLayoutNames()
+        initTabLayoutSelectedListener()
+        initOnScrollChangeLister()
+        setOtherTabLayout()
     }
 
-    private fun initTabLayoutScrollListener() {
-        binding.tabHatDetail.addOnTabSelectedListener(this)
-        binding.svHatDetail.setOnScrollChangeListener(this)
-    }
+    private fun initTabLayoutSelectedListener() {
+        binding.tabHatDetail.setDetailTabNames()
+        binding.tabHatDetail.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab?.position) {
+                    0 -> binding.svHatDetail.smoothScrollToView(binding.layoutInfo)
+                    1 -> binding.svHatDetail.smoothScrollToView(binding.layoutSize)
+                    2 -> binding.svHatDetail.smoothScrollToView(binding.layoutRecommend)
+                    3 -> binding.svHatDetail.smoothScrollToView(binding.layoutReview)
+                    4 -> binding.svHatDetail.smoothScrollToView(binding.layoutAsk)
+                }
+                binding.appbarHatDetail.setExpanded(false)
+            }
 
-    private fun setTabLayoutNames() {
-        with(binding) {
-            tabHatDetail.setDetailTabNames()
-            tabHatDetailSize.setDetailTabNames()
-            tabHatDetailSize.setScrollPosition(1, 0f, false)
-            tabHatDetailRec.setDetailTabNames()
-            tabHatDetailRec.setScrollPosition(2, 0f, false)
-            tabHatDetailReview.setDetailTabNames()
-            tabHatDetailReview.setScrollPosition(3, 0f, false)
-            tabHatDetailAsk.setDetailTabNames()
-            tabHatDetailAsk.setScrollPosition(4, 0f, false)
-        }
+            override fun onTabUnselected(tab: TabLayout.Tab?) = Unit
+            override fun onTabReselected(tab: TabLayout.Tab?) = Unit
+        })
     }
 
     private fun TabLayout.setDetailTabNames() {
@@ -48,45 +47,47 @@ class HatDetailActivity : BindingActivity<ActivityHatDetailBinding>(R.layout.act
         }
     }
 
-    override fun onTabSelected(tab: TabLayout.Tab?) {
-        when (tab?.position) {
-            0 -> binding.svHatDetail.smoothScrollToView(binding.layoutInfo)
-            1 -> binding.svHatDetail.smoothScrollToView(binding.layoutSize)
-            2 -> binding.svHatDetail.smoothScrollToView(binding.layoutRecommend)
-            3 -> binding.svHatDetail.smoothScrollToView(binding.layoutReview)
-            4 -> binding.svHatDetail.smoothScrollToView(binding.layoutAsk)
+    private fun initOnScrollChangeLister() {
+        binding.svHatDetail.setOnScrollChangeListener { _, _, scrollY, _, _ ->
+            when {
+                scrollY < binding.svHatDetail.computeDistanceToView(binding.layoutSize) -> {
+                    binding.tabHatDetail.setScrollPosition(0, 0f, true)
+                }
+
+                scrollY >= binding.svHatDetail.computeDistanceToView(binding.layoutSize) &&
+                        scrollY < binding.svHatDetail.computeDistanceToView(binding.layoutRecommend) -> {
+                    binding.tabHatDetail.setScrollPosition(1, 0f, true)
+                }
+
+                scrollY >= binding.svHatDetail.computeDistanceToView(binding.layoutRecommend) &&
+                        scrollY < binding.svHatDetail.computeDistanceToView(binding.layoutReview) -> {
+                    binding.tabHatDetail.setScrollPosition(2, 0f, true)
+                }
+
+                scrollY >= binding.svHatDetail.computeDistanceToView(binding.layoutReview) &&
+                        scrollY < binding.svHatDetail.computeDistanceToView(binding.layoutAsk) -> {
+                    binding.tabHatDetail.setScrollPosition(3, 0f, true)
+                }
+
+                scrollY >= binding.svHatDetail.computeDistanceToView(binding.layoutAsk) -> {
+                    binding.tabHatDetail.setScrollPosition(4, 0f, true)
+                }
+            }
         }
-        binding.appbarHatDetail.setExpanded(false)
     }
 
-    override fun onTabUnselected(tab: TabLayout.Tab?) = Unit
-    override fun onTabReselected(tab: TabLayout.Tab?) = Unit
-
-
-    override fun onScrollChange(p0: View?, p1: Int, scrollY: Int, p3: Int, p4: Int) {
-        when {
-            scrollY < binding.svHatDetail.computeDistanceToView(binding.layoutSize) -> {
-                binding.tabHatDetail.setScrollPosition(0, 0f, true)
-            }
-
-            scrollY >= binding.svHatDetail.computeDistanceToView(binding.layoutSize) &&
-                    scrollY < binding.svHatDetail.computeDistanceToView(binding.layoutRecommend) -> {
-                binding.tabHatDetail.setScrollPosition(1, 0f, true)
-            }
-
-            scrollY >= binding.svHatDetail.computeDistanceToView(binding.layoutRecommend) &&
-                    scrollY < binding.svHatDetail.computeDistanceToView(binding.layoutReview) -> {
-                binding.tabHatDetail.setScrollPosition(2, 0f, true)
-            }
-
-            scrollY >= binding.svHatDetail.computeDistanceToView(binding.layoutReview) &&
-                    scrollY < binding.svHatDetail.computeDistanceToView(binding.layoutAsk) -> {
-                binding.tabHatDetail.setScrollPosition(3, 0f, true)
-            }
-
-            scrollY >= binding.svHatDetail.computeDistanceToView(binding.layoutAsk) -> {
-                binding.tabHatDetail.setScrollPosition(4, 0f, true)
-            }
+    private fun setOtherTabLayout() {
+        with(binding) {
+            tabHatDetailSize.initNotWorkingTabLayout(1)
+            tabHatDetailRec.initNotWorkingTabLayout(2)
+            tabHatDetailReview.initNotWorkingTabLayout(3)
+            tabHatDetailAsk.initNotWorkingTabLayout(4)
         }
+    }
+
+    private fun TabLayout.initNotWorkingTabLayout(selectedTab: Int) {
+        setDetailTabNames()
+        setScrollPosition(selectedTab, 0f, true)
+        touchables.forEach { it.isClickable = false }
     }
 }
