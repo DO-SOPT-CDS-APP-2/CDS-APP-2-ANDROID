6a26d740dc4fc9ae9d005eb4258aae449c5d6f00
LEE YOU BIN <youbeene3409@naver.com>
2023-11-23 04:44:17 +0900

    [FEAT/#10] chip 구현

--- app/src/main/java/org/sopt/cds29cm/presentation/home/HomeFragment.kt
+++ app/src/main/java/org/sopt/cds29cm/presentation/home/HomeFragment.kt
@@ -1,19 +1,28 @@
 package org.sopt.cds29cm.presentation.home
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import org.sopt.cds29cm.R
+import android.view.WindowManager
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import org.sopt.cds29cm.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private var _binding : FragmentHomeBinding? = null
+    private val binding get()= _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
