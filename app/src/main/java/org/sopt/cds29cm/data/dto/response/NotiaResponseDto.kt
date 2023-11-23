9213f5b20a2e223ecdd50570258d47851d1cc443
LEE YOU BIN <youbeene3409@naver.com>
2023-11-24 05:40:58 +0900

    [ADD/#10] 상품 정보 Dto 파일 추가

---
+++ app/src/main/java/org/sopt/cds29cm/data/dto/response/NotiaResponseDto.kt
@@ -0,0 +1,22 @@
+package org.sopt.cds29cm.data.dto.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class NotiaResponseDto(
+    @SerialName("imageUrl")
+    val imageUrl: String,
+    @SerialName("brand")
+    val brand: String,
+    @SerialName("name")
+    val name: String,
+    @SerialName("discountRate")
+    val discountRate: Int,
+    @SerialName("discountedPrice")
+    val discountedPrice: Int,
+    @SerialName("like")
+    val like: Boolean,
+    @SerialName("likeCount")
+    val likeCount: Int,
+)
