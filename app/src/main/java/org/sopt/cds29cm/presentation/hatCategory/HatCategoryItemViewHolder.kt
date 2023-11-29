cd69d5897bbbf41a541e942569e179f6992c8672
Choi Min Young <codingmy@naver.com>
2023-11-29 20:28:44 +0900

    [FEAT/#19] 할인율에 따라 할인율 가시 기능 구현

--- app/src/main/java/org/sopt/cds29cm/presentation/hatCategory/HatCategoryItemViewHolder.kt
+++ app/src/main/java/org/sopt/cds29cm/presentation/hatCategory/HatCategoryItemViewHolder.kt
@@ -1,8 +1,10 @@
 package org.sopt.cds29cm.presentation.hatCategory
 
 import android.icu.text.DecimalFormat
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import org.sopt.cds29cm.R
 import org.sopt.cds29cm.data.dataclass.HatCategoryItemComment
 import org.sopt.cds29cm.data.dto.response.ResponseCategoryItemDTO
 import org.sopt.cds29cm.databinding.ItemHatCategoryVerticalBinding
@@ -14,7 +16,10 @@ class HatCategoryItemViewHolder(private val binding: ItemHatCategoryVerticalBind
             ivHatCategoryVerticalItemImage.load(itemDataList.imageUrl)
             tvHatCategoryVerticalItemBrand.text = itemDataList.brand
             tvHatCategoryVerticalItemName.text = itemDataList.name
-            tvHatCategoryVerticalItemDiscount.text = itemDataList.discount.toString() + "%"
+            if (itemDataList.discount != 0){
+                tvHatCategoryVerticalItemDiscount.text = itemDataList.discount.toString() + "%"
+            } else
+                tvHatCategoryVerticalItemDiscount.setPadding(0,0,0,0)
             val decimal = DecimalFormat("#,###")
             tvHatCategoryVerticalItemPrice.text = decimal.format(itemDataList.price)
             tvHatCategoryVerticalItemHeart.text = commentDataList.heart
