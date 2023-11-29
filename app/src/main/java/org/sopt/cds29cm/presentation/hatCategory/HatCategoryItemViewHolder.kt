53813f505bed8c99bee910b49c6978da6ac179a1
Choi Min Young <codingmy@naver.com>
2023-11-30 01:12:11 +0900

    [FEAT/#19] viewHolder에 각 아이템 인식 구현

--- app/src/main/java/org/sopt/cds29cm/presentation/hatCategory/HatCategoryItemViewHolder.kt
+++ app/src/main/java/org/sopt/cds29cm/presentation/hatCategory/HatCategoryItemViewHolder.kt
@@ -2,6 +2,7 @@ package org.sopt.cds29cm.presentation.hatCategory
 
 import android.icu.text.DecimalFormat
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import org.sopt.cds29cm.R
@@ -16,16 +17,42 @@ class HatCategoryItemViewHolder(private val binding: ItemHatCategoryVerticalBind
             ivHatCategoryVerticalItemImage.load(itemDataList.imageUrl)
             tvHatCategoryVerticalItemBrand.text = itemDataList.brand
             tvHatCategoryVerticalItemName.text = itemDataList.name
-            if (itemDataList.discount != 0){
+            if (itemDataList.discount != 0) {
                 tvHatCategoryVerticalItemDiscount.text = itemDataList.discount.toString() + "%"
             } else
-                tvHatCategoryVerticalItemDiscount.setPadding(0,0,0,0)
+                tvHatCategoryVerticalItemDiscount.setPadding(0, 0, 0, 0)
             val decimal = DecimalFormat("#,###")
             tvHatCategoryVerticalItemPrice.text = decimal.format(itemDataList.price)
             tvHatCategoryVerticalItemHeart.text = commentDataList.heart
             tvHatCategoryVerticalItemMark.text = commentDataList.mark
             tvHatCategoryVerticalItemMarkPeople.text = commentDataList.markPeople
+            if (itemDataList.brand.toString() == "시오르")
+                ivHatCategoryVerticalItemHeart.setImageResource(R.drawable.ic_like_on_black_24dp)
+
+/*
+            val listener = View.OnClickListener {
+                itemDataList.let {
+                    onClick(itemDataList)
+                }
+            }
+            ivHatCategoryVerticalItemHeart.setOnClickListener(listener)
+*/
+
+            //일단 각자 아이템 클릭 인식 가능, id 추출 가능
+            ivHatCategoryVerticalItemHeart.setOnClickListener { // 클릭 이벤트 리스너 처리
+                Toast.makeText(
+                    it.context,
+                    "ViewHolder Clicked -> ID : ${itemDataList.name}",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+
+
+            }
+
+
         }
     }
 
+    }
 }
