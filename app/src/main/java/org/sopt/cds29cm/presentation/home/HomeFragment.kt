ca34aa4fb19b352dc681aa912e22b22e85fb26fd
Sangho Kim <marchbreeze03@gmail.com>
2023-12-01 18:11:58 +0900

    [FIX/#25] HomeFragment 수정

--- app/src/main/java/org/sopt/cds29cm/presentation/home/HomeFragment.kt
+++ app/src/main/java/org/sopt/cds29cm/presentation/home/HomeFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.LiveData
 import org.sopt.cds29cm.data.dto.response.HomeResponseDto
 import org.sopt.cds29cm.databinding.FragmentHomeBinding
 import org.sopt.cds29cm.module.ServicePool.homeService
@@ -17,6 +16,7 @@ class HomeFragment : Fragment() {
     private val binding: FragmentHomeBinding get() = requireNotNull(_binding) { "바인딩 객체가 생성되지 않음" }
 
     private val viewModel: HomeViewModel by viewModels { HomeViewModelFactory(homeService) }
+
     private lateinit var dataAdapter: HomeNotiaAdapter
 
     override fun onCreateView(
@@ -31,7 +31,6 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initNotiaAdapter()
-
         initRecommendAdapter()
         initIssueAdapter()
         initPopularAdapter()
