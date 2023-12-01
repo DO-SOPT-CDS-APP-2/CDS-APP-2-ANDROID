e7bbedfa2ab9c4885ab65464667b90bb2ca147d4
Choi Min Young <codingmy@naver.com>
2023-12-01 14:13:44 +0900

    [FEAT/#23] 왼쪽 상위카테고리 세부 UI 수정

--- app/src/main/java/org/sopt/cds29cm/presentation/category/CategoryVerticalLeftViewHolder.kt
+++ app/src/main/java/org/sopt/cds29cm/presentation/category/CategoryVerticalLeftViewHolder.kt
@@ -1,5 +1,7 @@
 package org.sopt.cds29cm.presentation.category
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.recyclerview.widget.RecyclerView
 import org.sopt.cds29cm.R
 import org.sopt.cds29cm.data.dataclass.CategoryVertical
@@ -7,16 +9,15 @@ import org.sopt.cds29cm.databinding.ItemCategoryVerticalLeftBinding
 
 class CategoryVerticalLeftViewHolder(private val binding: ItemCategoryVerticalLeftBinding) :
     RecyclerView.ViewHolder(binding.root) {
+    @SuppressLint("ResourceAsColor")
     fun onBind(categoryVerticalData: CategoryVertical) {
         with(binding){
             tvCategoryVerticalLeft.text = categoryVerticalData.categoryName
+            if (categoryVerticalData.categoryName == "액세서리") {
+                tvCategoryVerticalLeft.setTextAppearance(R.style.TextAppearance_Kor_Bold)
+                itemCategoryVerticalLeft.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            }
         }
- //       binding.
-/*
-        if (categoryVerticalData.categoryName.toString() == "악세서리") {
-            binding.layoutCategoryVerticalLeft.setBackgroundColor(Color.parseColor("#FFFFFF"))
-        }
-*/
     }
 }
 
